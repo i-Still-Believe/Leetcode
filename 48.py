57de6d187ca95ca66375b9726f32717d4452abaf
7 <jiaguo@student.ethz.ch>
2022-10-21 23:17:37 +0200

    update 48

---
+++ 48.py
@@ -0,0 +1,26 @@
+from typing import List
+import copy
+class Solution:
+    # def rotate(self, matrix: List[List[int]]) -> None:
+    #     """
+    #     Do not return anything, modify matrix in-place instead.
+    #     """
+    #     matrix_tmp = copy.deepcopy(matrix)
+    #     n = len(matrix)
+    #     for i in range(n):
+    #         for j in range(n):
+    #             matrix[i][j] = matrix_tmp[n - 1 - j][i]
+        # print(matrix)
+
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for i in range(n // 2 + 1):
+            for j in range(i, n - i - 1):
+                tmp = matrix[i][j]
+                matrix[i][j] = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = tmp
+        print(matrix)
+sol = Solution()
+sol.rotate([[1,2,3],[4,5,6],[7,8,9]])
